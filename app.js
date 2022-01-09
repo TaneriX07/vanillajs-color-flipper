f22ed5a23502a82cb45ded84cc8ca65876da67df
TaneriX07 <tanerivince@gmail.com>
2022-01-09 21:23:09 +0800

    Add support to submit color using keyboard
    By encapsulating input box and button inside <form>, we can now use the 'ENTER' key to submit the color we typed.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+const form = document.getElementById('form');
 
-btn.addEventListener('click', () => {
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
     colorCode = document.getElementById('text').value;
     document.body.style.backgroundColor = colorCode;
     color.textContent = colorCode;
+
+    //clear input
+    document.getElementById('text').value = '';
 })
